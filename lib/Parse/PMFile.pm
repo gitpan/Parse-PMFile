84d25285def57d421492bcb5cb63b315b953fb4a
Kenichi Ishigaki <ishigaki@cpan.org>
2013-05-13 06:25:48 +0000

    Import of ISHIGAKI/Parse-PMFile-0.02 from CPAN.
    gitpan-cpan-distribution: Parse-PMFile
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ISHIGAKI/Parse-PMFile-0.02.tar.gz
    gitpan-cpan-author:       ISHIGAKI
    gitpan-cpan-maturity:     released

--- lib/Parse/PMFile.pm
+++ lib/Parse/PMFile.pm
@@ -11,7 +11,7 @@ use File::Spec ();
 use File::Temp ();
 use POSIX ':sys_wait_h';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 our $VERBOSE = 0;
 
 sub new {
@@ -227,14 +227,14 @@ sub _packages_per_pmfile {
 
         if (
             $pline =~ m{
-                      (.*)
+                      # (.*) # takes too much time if $pline is long
                       \bpackage\s+
                       ([\w\:\']+)
                       \s*
                       (?: $ | [\}\;] | \s+($version::STRICT) )
                     }x) {
-            $pkg = $2;
-            $strict_version = $3;
+            $pkg = $1;
+            $strict_version = $2;
             if ($pkg eq "DB"){
                 # XXX if pumpkin and perl make him comaintainer! I
                 # think I always made the pumpkins comaint on DB
